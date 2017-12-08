a08ce7bc5f5199deed04f2815fde38fa543b1bfa
manukfarmers <mahhov@yahoo.com>
2017-12-07 18:36:58 -0800

    integrate with psql database and modify server.js to track each visit

---
+++ migrations/20171207000000-createTables.js
@@ -0,0 +1,27 @@
+let async = require('async');
+
+let dbm;
+let type;
+let seed;
+
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+    return db.createTable('visitor', {
+        date: TIMESTAMP
+    });
+};
+
+exports.down = function (db, callback) {
+    return db.dropTable('visitor').then(() => {
+        db.dropTable('authentication');
+    });
+};
+
+exports._meta = {
+    'version': 1
+};
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/public'));
-// Start the app by listening on the default
-// Heroku port
+const pgp = require('pg-promise')();
+const db = pgp(process.env.DATABASE_URL || 'postgres://manukhovanesian@localhost:5432/jsonBeautifyAngular');
+
+var incrementCount = () => {
+    db.none('INSERT INTO visitor (date) VALUES (CURRENT_TIMESTAMP)').catch((err) => {
+        console.log('error updating visitor table:');
+        console.log(err);
+    });
+};
+
+var wrapper = (req, res, next) => {
+    if (req.url === '/')
+        incrementCount();
+    express.static(__dirname + '/public')(req, res, next);
+};
+
+app.use(wrapper);
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.get('/count', (req, res) => {
+    db.one('SELECT count(*) from count').then((count) => {
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(200).send(count);
+    }).catch(() => {
+        res.status(400).send();
+    });
+});
+
 app.listen(process.env.PORT || 8085);
